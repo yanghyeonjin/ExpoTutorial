c92614c5ef800a3ca536bd6bbd2274528d2a38db
yanghyeonjin <guswls7081@gmail.com>
2025-01-08 17:21:58 +0900

    StyleSheet와 텍스트 변경

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,21 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View
-      style={styles.container}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Home Screen</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-container: {
-  flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-}
-})
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+});
