b586fe3f6c8e3e74254b09d492d1bdac2cd6fdc7
yanghyeonjin <guswls7081@gmail.com>
2025-01-08 17:12:26 +0900

    ESLint, Prettier 설정

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,19 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+container: {
+  flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+}
+})
